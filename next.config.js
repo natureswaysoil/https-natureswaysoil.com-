704decbc35e0aced969f4246c08ebb4be9bd14b8
natureswaysoil <natureswaysoil@gmail.com>
2025-08-20 15:22:37 -0400

    Rebuild Next.js site with Stripe checkout

--- next.config.js
+++ next.config.js
@@ -1,20 +1,8 @@
-// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'm.media-amazon.com' }
-    ]
-  }
-};
-module.exports = nextConfig;
-
-// next.config.js
-module.exports = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'm.media-amazon.com' }
-    ],
+    remotePatterns: [{ protocol: 'https', hostname: 'm.media-amazon.com' }],
   },
 };
 
+module.exports = nextConfig;
