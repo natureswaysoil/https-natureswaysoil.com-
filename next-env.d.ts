b1873742431d728e631a8a41659cb6bb1f8eabcd
natureswaysoil <natureswaysoil@gmail.com>
2025-08-24 17:14:31 -0400

    Update TypeScript configuration link in next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,4 +1,4 @@
- (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
+  (cd "$(git rev-parse --show-toplevel)" && git apply --3way <<'EOF' 
 diff --git a/next-env.d.ts b/next-env.d.ts
 index 4f11a03dc6cc37f2b5105c08f2e7b24c603ab2f4..725dd6f245153034160edeb040f0846109714ce3 100644
 --- a/next-env.d.ts
